5fcc6a5663eaf36993d2274f0c863983cc065a68
Santiii4 <Sal.sanchez@alumno.etec.um.edu.ar>
2023-08-23 12:33:50 -0300

    23/08/23

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter />
+        <Routes />
+          <Route path = '/home' elements= {<>
+            <Header />
+            <Footer />
+            </>
+          } />
       <Header />
       <Footer />
     </div>
